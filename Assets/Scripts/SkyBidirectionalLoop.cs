9c405205101a72ee451c4112fbf0e5a4651caabf
CollegeGamerStudent <769404@newcollege.ac.uk>
2026-01-15 10:00:54 +0000

    made it so you see the sky clearly

---
+++ Assets/Scripts/SkyBidirectionalLoop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SkyBidirectionalLoop : MonoBehaviour
+{
+    Camera cam;
+    const float width = 14.63f;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float camX = cam.transform.position.x;
+
+        // too far left → move to the right
+        if (transform.position.x < camX - width)
+        {
+            transform.position += Vector3.right * width * 2f;
+        }
+
+        // too far right → move to the left
+        if (transform.position.x > camX + width)
+        {
+            transform.position += Vector3.left * width * 2f;
+        }
+    }
+}
