ee10d26ea64f861c05e7bd049ddd3e0426fb6707
CollegeGamerStudent <769404@newcollege.ac.uk>
2026-01-21 09:35:10 +0000

    fixed scene script

--- Assets/Scripts/SceneTrigger.cs
+++ Assets/Scripts/SceneTrigger.cs
@@ -11,7 +11,7 @@ public class SceneTrigger : MonoBehaviour
 
     public string sceneToLoad;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
 
     {
 
